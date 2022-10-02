e16ce22e19b5a7a2d67c6e92457e74606338fce9
Nicolas Ferioli <feriolinicolas@gmail.com>
2022-10-02 20:11:25 -0300

    carrousel v2

--- PracticoEntregable2/js/carrousel.js
+++ PracticoEntregable2/js/carrousel.js
@@ -1,12 +1,13 @@
 let botonesSig = document.querySelectorAll('.btnCarrouselSig');
 let botonesAtras= document.querySelectorAll('.btnCarrouselAtras');
-const numeroDePaginas = 3;// numero de paginas por carrousel, si el numero de paginas cambia en el dom, debemos cambiarlo manualmente aca
+const cardsPorPAgina = 3;// numero de cards por pagina, si el numero de cards cambia en el dom, debemos cambiarlo manualmente aca
 
 for(let i=0;i<botonesSig.length;i++){
     botonesSig[i].addEventListener("click", function(){
         let ultimaClase = botonesSig[i].classList[botonesSig[i].classList.length-1];
         if(ultimaClase != "btnApagado"){
             let li = botonesSig[i].previousElementSibling.children;
+            let cantidadDePaginas = Math.ceil(li.length / cardsPorPAgina);
             let liClaseAcual = li[0].classList[0];
             let paginaActual = parseInt(liClaseAcual.charAt(liClaseAcual.length-1))+1;
 
@@ -15,7 +16,7 @@ for(let i=0;i<botonesSig.length;i++){
                 li[j].classList.add("pagina"+paginaActual);
             }
 
-            if(paginaActual == numeroDePaginas){
+            if(paginaActual == cantidadDePaginas){
                 botonesSig[i].classList.add("btnApagado");
             }
             if(paginaActual != 1){
@@ -30,6 +31,7 @@ for(let i=0;i<botonesAtras.length;i++){
         let ultimaClase = botonesAtras[i].classList[botonesAtras[i].classList.length-1];
         if(ultimaClase != "btnApagado"){
             let li = botonesAtras[i].nextElementSibling.children;
+            let cantidadDePaginas = Math.ceil(li.length / cardsPorPAgina);
             let liClaseAcual = li[0].classList[0];
             let paginaActual = parseInt(liClaseAcual.charAt(liClaseAcual.length-1))-1;
 
@@ -41,7 +43,7 @@ for(let i=0;i<botonesAtras.length;i++){
             if(paginaActual == 1){
                 botonesAtras[i].classList.add("btnApagado");
             }
-            if(paginaActual != numeroDePaginas){
+            if(paginaActual != cantidadDePaginas){
                 botonesAtras[i].nextElementSibling.nextElementSibling.classList.remove("btnApagado");
             }
         }
