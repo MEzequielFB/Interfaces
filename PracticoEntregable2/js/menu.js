d59e23b60b016abf1fe7363119fbbd3d18a5ee51
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-10-05 13:39:17 -0300

    Agrega menus desplegables

--- PracticoEntregable2/js/menu.js
+++ PracticoEntregable2/js/menu.js
@@ -1,6 +1,28 @@
-function mostrarMenu() {
-    document.querySelector("nav").classList.add("visible");
+function mostrarMenu(menu) {
+    menu.classList.add("visible");
 }
 
-let menu_hamburguesa = document.querySelector(".menu-hamburguesa");
-menu_hamburguesa.addEventListener("click", mostrarMenu);
+function ocultarMenu(menu) {
+    menu.classList.remove("visible");
+}
+
+let menu = document.querySelector(".menu");
+let menu_perfil = document.querySelector(".menu-perfil");
+
+let icono_menu_hamburguesa = document.querySelector(".icono-menu-hamburguesa");
+icono_menu_hamburguesa.addEventListener("click", function() {
+    mostrarMenu(menu);
+});
+let cruz_btn = document.querySelector(".cruz-menu");
+cruz_btn.addEventListener("click", function() {
+    ocultarMenu(menu);
+});
+
+let perfil_header = document.querySelector(".perfil-header");
+perfil_header.addEventListener("click", function() {
+    if (!menu_perfil.classList.contains("visible")) {
+        mostrarMenu(menu_perfil);
+    } else {
+        ocultarMenu(menu_perfil);
+    }
+});
