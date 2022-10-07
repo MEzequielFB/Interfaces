0f2b21629e8262fdd3cd4efc1f8f89b47cb9e40f
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-10-07 17:43:40 -0300

    Agrega precio, tags y hover a los juegos mas jugados

--- PracticoEntregable2/js/cards.js
+++ PracticoEntregable2/js/cards.js
@@ -9,3 +9,15 @@ for (let i = 0; i < contenedoresCards.length; i++) {
         });
     }
 }
+
+let cardsMasJugados = document.querySelectorAll(".cards-mas-jugados");
+for (let i = 0; i < cardsMasJugados.length; i++) {
+    for (let j = 0; j < cardsMasJugados[i].children.length; j++) {
+        cardsMasJugados[i].children[j].addEventListener("mouseover", function(){
+            cardsMasJugados[i].children[j].lastElementChild.classList.add("visible");
+        });
+        cardsMasJugados[i].children[j].addEventListener("mouseout", function(){
+            cardsMasJugados[i].children[j].lastElementChild.classList.remove("visible");
+        });
+    }
+}
