0a27649ca7b757b3b578fae8fe5d709c27e477b8
Nicolas Ferioli <feriolinicolas@gmail.com>
2022-10-07 23:13:04 -0300

    terminado

--- PracticoEntregable2/js/cards.js
+++ PracticoEntregable2/js/cards.js
@@ -2,10 +2,10 @@ let contenedoresCards = document.querySelectorAll(".contenedorCards");
 for (let i = 0; i < contenedoresCards.length; i++) {
     for (let j = 0; j < contenedoresCards[i].children.length; j++) {
         contenedoresCards[i].children[j].addEventListener("mouseover", function(){
-            contenedoresCards[i].children[j].lastElementChild.classList.add("visible");
+            contenedoresCards[i].children[j].firstElementChild.lastElementChild.classList.add("visible");
         });
         contenedoresCards[i].children[j].addEventListener("mouseout", function(){
-            contenedoresCards[i].children[j].lastElementChild.classList.remove("visible");
+            contenedoresCards[i].children[j].firstElementChild.lastElementChild.classList.remove("visible");
         });
     }
 }
