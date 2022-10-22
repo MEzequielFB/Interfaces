581da2f94ad389020e47aa797376dd75947a4a20
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-10-22 19:01:41 -0300

    Agrega archivos para el juego

--- PracticoEntregable3/js/ocultar-previa.js
+++ PracticoEntregable3/js/ocultar-previa.js
@@ -1,7 +1,6 @@
 document.addEventListener("click", function(){
     "use strict";
-
-    let canvas = document.querySelector(".canvas");
+    
     let previa_juego = document.querySelector(".juego");
 
     let btn_jugar = document.querySelector(".btn-jugar");
