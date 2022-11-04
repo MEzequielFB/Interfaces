1bc2f1870fb6e330788f8ec2b997d208f1637d52
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-11-04 14:18:45 -0300

    Cambia imagenes del cuatro en linea en el index y juegos mas jugados tienen links

--- PracticoEntregable3/js/cards.js
+++ PracticoEntregable3/js/cards.js
@@ -14,10 +14,10 @@ let cardsMasJugados = document.querySelectorAll(".cards-mas-jugados");
 for (let i = 0; i < cardsMasJugados.length; i++) {
     for (let j = 0; j < cardsMasJugados[i].children.length; j++) {
         cardsMasJugados[i].children[j].addEventListener("mouseover", function(){
-            cardsMasJugados[i].children[j].lastElementChild.classList.add("visible");
+            cardsMasJugados[i].children[j].lastElementChild.lastElementChild.classList.add("visible");
         });
         cardsMasJugados[i].children[j].addEventListener("mouseout", function(){
-            cardsMasJugados[i].children[j].lastElementChild.classList.remove("visible");
+            cardsMasJugados[i].children[j].lastElementChild.lastElementChild.classList.remove("visible");
         });
     }
 }
