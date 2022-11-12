7337c7b582e4f379e300c490bdd7c274c709bd3c
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-11-12 18:35:57 -0300

    Intentando hacer el punto 1 del tpe

--- PracticoEntregable4/js/menu-y-busqueda.js
+++ PracticoEntregable4/js/menu-y-busqueda.js
@@ -10,13 +10,7 @@ function setAnimationNameNone(elemento) {
     elemento.style.animationName = "none";
 }
 
-let menu = document.querySelector(".menu");
-let lista_btns_menu = document.querySelector(".lista-btns-menu");
-let menu_perfil = document.querySelector(".menu-perfil");
-
-let icono_menu_hamburguesa = document.querySelector(".icono-menu-hamburguesa");
-icono_menu_hamburguesa.addEventListener("click", function() {
-    mostrarElemento(menu);
+function mostrarItems() {
     let btns = lista_btns_menu.children;
     let tiempo_animacion = 1;
     for (let btn of btns) {
@@ -28,6 +22,49 @@ icono_menu_hamburguesa.addEventListener("click", function() {
             btn.firstElementChild.style.animation = `itemCarga ${tiempo_animacion}s ease-out forwards`;
             tiempo_animacion += 0.2;
         }, 0);
+    }
+}
+
+function ocultarItems() {
+    let btns = lista_btns_menu.children;
+    for (let btn of btns) {
+        setAnimationNameNone(btn);
+        setAnimationNameNone(btn.firstElementChild);
+
+        setTimeout(() => {
+            btn.style.animation = `itemCarga 1s linear forwards reverse`;
+            btn.firstElementChild.style.animation = `itemCarga 1s linear forwards reverse`;
+        }, 0);
+    }
+}
+
+let menu = document.querySelector(".menu");
+let lista_btns_menu = document.querySelector(".lista-btns-menu");
+let menu_perfil = document.querySelector(".menu-perfil");
+
+let icono_menu_hamburguesa = document.querySelector(".icono-menu-hamburguesa");
+let menu_hamburguesa_v2 = document.querySelector(".menu-hamburguesa-v2");
+menu_hamburguesa_v2.addEventListener("click", function() {
+    menu.classList.toggle("visible");
+    menu_hamburguesa_v2.classList.toggle("absoluto");
+    menu_hamburguesa_v2.style.animationName = "none";
+    if (menu_hamburguesa_v2.classList.contains("absoluto")) {
+        setTimeout(() => {
+            menu_hamburguesa_v2.style.animation = "menuHamburguesaTransicion .7s linear 0s 1 normal forwards";
+        }, 0);
+    } else {
+        setTimeout(() => {
+            menu_hamburguesa_v2.style.animation = "menuHamburguesaTransicion .7s linear 0s 1 reverse forwards";
+        }, 0);
+    }
+    if (menu.classList.contains("visible")) {
+        mostrarItems();
+    } else {
+        ocultarItems();
+    }
+    
+    for (let span of menu_hamburguesa_v2.children) {
+        span.classList.toggle("cruzTransicion");
     }
 });
 let cruz_btn = document.querySelector(".cruz-menu");
