2e8c7b43afcbb6bb005a4f3fe067048e96b78c7d
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-11-12 16:10:02 -0300

    Agrega animacion a los botones del menu

--- PracticoEntregable4/js/menu-y-busqueda.js
+++ PracticoEntregable4/js/menu-y-busqueda.js
@@ -6,16 +6,45 @@ function ocultarElemento(elemento) {
     elemento.classList.remove("visible");
 }
 
+function setAnimationNameNone(elemento) {
+    elemento.style.animationName = "none";
+}
+
 let menu = document.querySelector(".menu");
+let lista_btns_menu = document.querySelector(".lista-btns-menu");
 let menu_perfil = document.querySelector(".menu-perfil");
 
 let icono_menu_hamburguesa = document.querySelector(".icono-menu-hamburguesa");
 icono_menu_hamburguesa.addEventListener("click", function() {
     mostrarElemento(menu);
+    let btns = lista_btns_menu.children;
+    let tiempo_animacion = 1;
+    for (let btn of btns) {
+        /* btn.style.animationName = "none";
+        btn.firstElementChild.style.animationName = "none"; */
+        setAnimationNameNone(btn);
+        setAnimationNameNone(btn.firstElementChild);
+
+        setTimeout(() => {
+            btn.style.animation = `itemCarga ${tiempo_animacion}s ease-out forwards`;
+            btn.firstElementChild.style.animation = `itemCarga ${tiempo_animacion}s ease-out forwards`;
+            tiempo_animacion += 0.3;
+        }, 0);
+    }
 });
 let cruz_btn = document.querySelector(".cruz-menu");
 cruz_btn.addEventListener("click", function() {
     ocultarElemento(menu);
+    let btns = lista_btns_menu.children;
+    for (let btn of btns) {
+        setAnimationNameNone(btn);
+        setAnimationNameNone(btn.firstElementChild);
+        
+        setTimeout(() => {
+            btn.style.animation = `itemCarga 1s linear forwards reverse`;
+            btn.firstElementChild.style.animation = `itemCarga 1s linear forwards reverse`;
+        }, 0);
+    }
 });
 
 let perfil_header = document.querySelector(".perfil-header");
