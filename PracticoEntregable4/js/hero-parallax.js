5fc71aefc32015d222bed3d2c9a4885cf51a4503
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-11-16 15:07:08 -0300

    Mueve los personajes al scrollear

---
+++ PracticoEntregable4/js/hero-parallax.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+    "use strict";
+
+    function onScroll() {
+        let posicion_scroll = window.scrollY;
+        console.log(posicion_scroll);
+        personaje1.style.bottom = - (posicion_scroll * 0.4) + "px";
+        personaje1.style.left = - (posicion_scroll * 0.2) + "px";
+        personaje2.style.bottom = - (posicion_scroll * 0.4) + "px";
+        personaje2.style.right = - (posicion_scroll * 0.2) + "px";
+    }
+
+    const titulo_juego = document.querySelector(".titulo-juego");
+    const personaje1 = document.querySelector(".personaje1");
+    const personaje2 = document.querySelector(".personaje2");
+
+    document.addEventListener("scroll", onScroll);
+});
